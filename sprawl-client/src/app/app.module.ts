12b72e1d8f66e04053cbf613c79f016f32abf8bc
Thomas <thomas99k@gmail.com>
2019-04-16 17:21:00 -0400

    core - add page animations

--- sprawl-client/src/app/app.module.ts
+++ sprawl-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { LogoutComponent } from './logout/logout.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [LoggedInGuardService],
   bootstrap: [AppComponent]
