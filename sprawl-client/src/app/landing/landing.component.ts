bb469a34d77ed5562b8152133ea58a30a4b7330c
Thomas <thomas99k@gmail.com>
2019-04-24 11:00:11 -0400

    patch - make login button on landing page router based

--- sprawl-client/src/app/landing/landing.component.ts
+++ sprawl-client/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
+  login() {
+      this.router.navigateByUrl('/login');
+  }
 }
