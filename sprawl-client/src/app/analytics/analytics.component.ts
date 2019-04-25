8e2bb01d75ff4e54d16a3db2623af6add16e9d0f
Thomas <thomas99k@gmail.com>
2019-04-25 03:41:48 -0400

    core - add basic task estimation and task completion time series charts to analytics view

--- sprawl-client/src/app/analytics/analytics.component.ts
+++ sprawl-client/src/app/analytics/analytics.component.ts
@@ -4,18 +4,50 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.css']
+  styleUrls: ['./analytics.component.css'],
 })
 export class AnalyticsComponent implements OnInit {
 
   private tasksUrl = 'http://localhost:8080/api/task/stats';
 
   private data : any;
+  private tsEstData: any;
+  private tsTotalData: any;
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Time';
+  showYAxisLabel = true;
+  yAxisLabel = 'Est. Accuracy';
+  showGridLines = true;
+  maxXAxisTickLength = 10;
+  trimXAxisTicks = true;
+
+  tsEstDataX = 'Time';
+  tsEstDataY = 'Est. Accuracy'
+
+  tsTotalDataX = 'Time';
+  tsTotalDataY = 'Tasks Completed'
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getStats();
+    this.getTimeSeriesEstData();
+    this.getTimeSeriesTotalData();
   }
 
   getStats() {
@@ -25,6 +57,24 @@ export class AnalyticsComponent implements OnInit {
         'Authorization': localStorage.getItem('auth_token')
       }
     }).subscribe(e => this.data = e);
+  }
+
+  getTimeSeriesEstData() {
+    this.http.get(this.tasksUrl + '/timeseries/estimation', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('auth_token')
+      }
+    }).subscribe(e => this.tsEstData = e);
+  }
+
+  getTimeSeriesTotalData() {
+    this.http.get(this.tasksUrl + '/timeseries/totaltasks', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('auth_token')
+      }
+    }).subscribe(e => this.tsTotalData = e);
   }
 
   getDifferencePercentage(x: number, y: number): number {
--- sprawl-client/src/app/app.module.ts
+++ sprawl-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +34,8 @@ import { AnalyticsComponent } from './analytics/analytics.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxChartsModule
   ],
   providers: [LoggedInGuardService],
   bootstrap: [AppComponent]
