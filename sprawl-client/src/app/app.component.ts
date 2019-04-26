8f4f07ba9236eeefdec24ed4b316dc7f5621771e
Thomas <thomas99k@gmail.com>
2019-04-25 22:45:40 -0400

    patch - remove page transition animation system for aot prod

--- sprawl-client/src/app/app.component.ts
+++ sprawl-client/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { UserService } from './user.service';
 import { Router, RouterOutlet } from '@angular/router';
-import { routerTransition } from './router.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [routerTransition],
 })
 export class AppComponent {
   title = 'sprawl-client';
@@ -18,42 +16,4 @@ export class AppComponent {
 
   isLoggedIn = () => this.userService.isLoggedIn();
   currentRoute = () => this.router.url;
-  getPageTransition(routerOutlet: RouterOutlet) {
-    if (routerOutlet.isActivated) {
-      let transitionName = 'section';
-
-      const { path } = routerOutlet.activatedRoute.routeConfig;
-      const isSame = this.previousPath === path;
-      const isBackward = this.previousPath.startsWith(path);
-      const isForward = path.startsWith(this.previousPath);
-      const isLogin = path === '/login';
-      const isLogout = path === '/logout';
-      const isMain = path === '/main';
-
-      if (isSame) {
-        transitionName = 'none';
-      } else if (isLogin) {
-        transitionName = 'forward';
-      } else if (isLogout) {
-        transitionName = 'backward';
-      } else if (isMain) {
-        transitionName = 'forward';
-      } else if (isBackward && isForward) {
-        transitionName = 'initial';
-      } else if (isBackward) {
-        transitionName = 'backward';
-      } else if (isForward) {
-        transitionName = 'forward';
-      }
-
-      if (transitionName === 'none') {
-        transitionName = 'forward';
-      }
-
-      this.previousPath = path;
-
-      return transitionName;
-    }
-  }
-
 }
